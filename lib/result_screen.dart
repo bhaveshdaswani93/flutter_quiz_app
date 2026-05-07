fa047c01cd16f618574224ec72c3b99b8005b33a
Bhavesh Daswani <bhaveshdaswani93@gmail.com>
2026-05-07 14:11:22 +0530

    feat: add ResultSummary widget to display quiz results and user answers

--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_quiz_app/data/questions.dart';
+import 'package:flutter_quiz_app/result_summary.dart';
 
 class ResultScreen extends StatelessWidget {
   const ResultScreen(this.userAnswers, {super.key});
 
   final List<String> userAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (int i = 0; i < userAnswers.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].question,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': userAnswers[i],
+      });
+    }
+
+    return summary;
+  }
 
   void checkResult() {
     for (int i = 0; i < userAnswers.length; i++) {
@@ -18,18 +34,31 @@ class ResultScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text("Result Screen"),
-        Center(
-          child: Text('You did it!'),
-        ),
-        TextButton(
-          onPressed: checkResult,
-          child: Text('Restart Quiz'),
-        ),
-      ],
+    final List<Map<String, Object>> summaryData = getSummaryData();
+    final int numTotalQuestions = questions.length;
+    final int numCorrectQuestions = summaryData.where((data) {
+      return data['user_answer'] == data['correct_answer'];
+    }).length;
+
+    return Container(
+      margin: EdgeInsets.all(40),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "You answered $numCorrectQuestions out of $numTotalQuestions questions correctly!",
+          ),
+          SizedBox(height: 20),
+          ResultSummary(summaryData),
+          SizedBox(
+            height: 20,
+          ),
+          TextButton(
+            onPressed: checkResult,
+            child: Text('Restart Quiz'),
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ lib/result_summary.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/widgets.dart';
+
+class ResultSummary extends StatelessWidget {
+  const ResultSummary(this.summaryData, {super.key});
+
+  final List<Map<String, Object>> summaryData;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: summaryData.map((data) {
+            return Row(
+              children: [
+                Text(((data['question_index'] as int) + 1).toString()),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Text(data['question'] as String),
+                      Text(
+                        'Correct answer: ${data['correct_answer'] as String}',
+                      ),
+                      Text('Your answer: ${data['user_answer']}'),
+                    ],
+                  ),
+                ),
+              ],
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}

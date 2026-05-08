a714296cbde9adf924e069ea09caf3de14bbbac0
Bhavesh Daswani <bhaveshdaswani93@gmail.com>
2026-05-08 19:30:32 +0530

    feat: enhance ResultSummary widget with user answer display and styling improvements

--- lib/result_summary.dart
+++ lib/result_summary.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class ResultSummary extends StatelessWidget {
@@ -12,17 +13,51 @@ class ResultSummary extends StatelessWidget {
       child: SingleChildScrollView(
         child: Column(
           children: summaryData.map((data) {
+            final isCorrectAnswer =
+                data['user_answer'] == data['correct_answer'];
             return Row(
               children: [
-                Text(((data['question_index'] as int) + 1).toString()),
+                Container(
+                  height: 30,
+                  width: 30,
+                  alignment: Alignment.center,
+                  margin: const EdgeInsets.only(right: 10),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: isCorrectAnswer ? Colors.green : Colors.red,
+                  ),
+                  child: Text(
+                    ((data['question_index'] as int) + 1).toString(),
+
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      // backgroundColor: Colors.white,
+                    ),
+                  ),
+                ),
                 Expanded(
                   child: Column(
                     children: [
-                      Text(data['question'] as String),
+                      Text(
+                        data['question'] as String,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: const Color.fromARGB(214, 255, 255, 255),
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(
+                        'Your answer: ${data['user_answer']}',
+                        style: TextStyle(
+                          color: const Color.fromARGB(103, 255, 255, 255),
+                        ),
+                      ),
                       Text(
                         'Correct answer: ${data['correct_answer'] as String}',
+                        style: TextStyle(
+                          color: const Color.fromARGB(164, 48, 219, 85),
+                        ),
                       ),
-                      Text('Your answer: ${data['user_answer']}'),
                     ],
                   ),
                 ),
